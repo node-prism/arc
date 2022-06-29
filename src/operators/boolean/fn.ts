13c6d72e73da5bbda91a51d42d28717b50b421fb
Jonathan Pyers <jpyers@headspin.io>
2022-06-28 22:14:45 -0400

    Add `$fn` operator and test.

---
+++ src/operators/boolean/fn.ts
@@ -0,0 +1,27 @@
+import { ensureArray, isObject, Ok } from "../../utils";
+
+export function $fn(source: object, query: object): boolean {
+  let match = undefined;
+
+  if (isObject(query)) {
+    Ok(query).forEach((k) => {
+      if (isObject(query[k])) {
+        if (source[k] === undefined) return;
+        Ok(query[k]).forEach((j) => {
+          if (j === "$fn") {
+            match = true;
+            ensureArray(query[k][j]).forEach((fn) => {
+              if (!fn(source[k])) match = false;
+            });
+          }
+        });
+      }
+    });
+  }
+
+  if (match !== undefined) {
+    return match;
+  }
+
+  return false;
+}
---
+++ tests/specs/operators/boolean/fn.test.ts
@@ -0,0 +1,45 @@
+import { testSuite, expect } from "manten";
+import { nrml, testCollection } from "../../../common";
+
+export default testSuite(async ({ describe }) => {
+  describe("$fn", ({ test }) => {
+    test("works", () => {
+      const collection = testCollection();
+      collection.insert([
+        { a: 2 },
+        { a: 4 },
+        { a: 5 },
+        { a: 6 },
+      ]);
+      const isEven = (x: number) => x % 2 === 0;
+      const found = nrml(collection.find({ a: { $fn: isEven } }));
+      expect(found).toEqual([{ a: 2 }, { a: 4 }, { a: 6 }]);
+    });
+    test("works with nested", () => {
+      const collection = testCollection();
+      collection.insert([
+        { a: { b: { c: 2 } } },
+        { a: { b: { c: 4 } } },
+        { a: { b: { c: 5 } } },
+        { a: { b: { c: 6 } } },
+      ]);
+      const isEven = (x: number) => x % 2 === 0;
+      const found = nrml(collection.find({ c: { $fn: isEven } }));
+      expect(found).toEqual([ { a: { b: { c: 2 } } }, { a: { b: { c: 4 } } }, { a: { b: { c: 6 } } } ]);
+    });
+    test("multiple functions", () => {
+      const collection = testCollection();
+      collection.insert([
+        { a: 2 },
+        { a: 3 },
+        { a: 4 },
+        { a: 5 },
+        { a: 6 },
+      ]);
+      const isOdd = (x: number) => x % 2 !== 0;
+      const isThree = (x: number) => x === 3;
+      const found = nrml(collection.find({ a: { $fn: [isThree, isOdd] } }));
+      expect(found).toEqual([{ a: 3 }]);
+    });
+  });
+});
