ee7dcb311620cc292440831c2d69a93ca42c4e7f
nvms <pyersjonathan@gmail.com>
2022-09-10 22:39:32 -0600

    Fix `$not`, update test.

--- src/operators/boolean/not.ts
+++ src/operators/boolean/not.ts
@@ -1,15 +1,25 @@
-import { Ok, isObject, safeHasOwnProperty } from "../../utils";
+import { checkAgainstQuery } from "../../return_found";
+import { Ok, isObject, safeHasOwnProperty, ensureArray } from "../../utils";
 
 export function $not(source: object, query: object): boolean {
   const matches = [];
 
-  if (query["$not"]) {
-    if (isObject(query["$not"])) {
-      Ok(query["$not"]).forEach((k) => {
-        if (!safeHasOwnProperty(source, k)) return;
-        matches.push(source[k] !== query["$not"][k]);
-      });
-    }
+  if (isObject(query)) {
+    Ok(query).forEach((pk) => {
+      if (pk === "$not") {
+        let nots = query[pk];
+        nots = ensureArray(nots);
+        nots.forEach((not: object) => {
+          // if any of the keys in the `not` object don't exist in source, add false.
+          if (!Ok(not).every((notKey) => safeHasOwnProperty(source, notKey))) {
+            matches.push(false);
+            return;
+          }
+
+          matches.push(!checkAgainstQuery(source, not));
+        });
+      }
+    });
   }
 
   return matches.filter(Boolean).length > 0;
