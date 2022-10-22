d3951145805079c034388825efb416fc076644a5
nvms <pyersjonathan@gmail.com>
2022-10-22 12:13:49 -0600

    Early return here.

--- src/operators/boolean/or.ts
+++ src/operators/boolean/or.ts
@@ -6,23 +6,23 @@ export function $or(source: object, query: object): boolean {
 
   if (isObject(query)) {
     Ok(query).forEach((pk) => {
-      if (pk === "$or") {
-        let ors = query[pk];
-        ors = ensureArray(ors);
-        ors.forEach((or: object) => {
-          Ok(or).forEach((orKey) => {
-            // case: { num: (n) => n%2===0 }
-            if (
-              typeof or[orKey] === "function" &&
-              source[orKey] !== undefined
-            ) {
-              matches.push(or[orKey](source[orKey]));
-            } else {
-              matches.push(checkAgainstQuery(source, or));
-            }
-          });
+      if (pk !== "$or") return;
+
+      let ors = query[pk];
+      ors = ensureArray(ors);
+      ors.forEach((or: object) => {
+        Ok(or).forEach((orKey) => {
+          // case: { num: (n) => n%2===0 }
+          if (
+            typeof or[orKey] === "function" &&
+            source[orKey] !== undefined
+          ) {
+            matches.push(or[orKey](source[orKey]));
+          } else {
+            matches.push(checkAgainstQuery(source, or));
+          }
         });
-      }
+      });
     });
   }
 
