7dc8032d9743eb795f5d5322100a3f8211c2aa0f
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-01 23:10:32 -0700

    Return early if the property can be resolved.

--- src/return_found.ts
+++ src/return_found.ts
@@ -54,6 +54,9 @@ export function checkAgainstQuery(source: object, query: object): boolean {
         });
       }
 
+      if (key.includes(".")) {
+        return dot.get(source, key) === query[key];
+      }
       return (
         safeHasOwnProperty(source, key) &&
         checkAgainstQuery(source[key], query[key])
