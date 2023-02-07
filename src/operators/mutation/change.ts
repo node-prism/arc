ecd28485f76c3f8a8161c722e8b881aaab265bd5
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-06 19:50:56 -0700

    Slightly change `$change` behavior and update tests

--- src/operators/mutation/change.ts
+++ src/operators/mutation/change.ts
@@ -1,6 +1,7 @@
+import dot from "dot-wild";
 import { Collection } from "../..";
 import { changeProps } from "../../change_props";
-import { ensureArray, isObject, Ok } from "../../utils";
+import { ensureArray, isObject, Ok, unescapedFlatten } from "../../utils";
 
 export function $change<T>(
   source: T[],
@@ -12,17 +13,31 @@ export function $change<T>(
 
   mods.forEach((mod) => {
     if (!isObject(mod) && !Array.isArray(mod)) {
-      const obj = {};
+      const flattened = unescapedFlatten(query);
 
-      Ok(query).forEach((key) => {
-        obj[key] = mod;
+      Ok(flattened).forEach((key) => {
+        source = source.map((doc: T) => {
+          if (dot.get(doc, key) !== undefined) return dot.set(doc, key, mod[key]);
+          return doc;
+        });
       });
 
-      source = changeProps(source, query, obj, false);
-    } else {
-      source = changeProps(source, query, mod, false);
+      return;
     }
-  })
+
+    if (isObject(mod)) {
+      Ok(mod).forEach((key) => {
+        source = source.map((doc: T) => {
+          if (dot.get(doc, key) !== undefined) return dot.set(doc, key, mod[key]);
+          return doc;
+        });
+      });
+
+      return;
+    }
+
+    source = changeProps(source, query, mod, false);
+  });
 
   return source;
 }
