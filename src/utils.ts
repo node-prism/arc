99c537cf14056e475ec8c0391e4023ebeacc1ebb
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-26 16:04:43 -0600

    `2.1.8`. Avoid using `path`.

--- src/utils.ts
+++ src/utils.ts
@@ -58,3 +58,13 @@ export function unescapedFlatten(o: object) {
 
 
 export const isNode = () => typeof process !== "undefined" && process.versions != null && process.versions.node != null;
+
+export const buildPath = (...args: string[]) => {
+  return args.map((part, i) => {
+    if (i === 0) {
+      return part.trim().replace(/[\/]*$/g, '')
+    } else {
+      return part.trim().replace(/(^[\/]*|[\/]*$)/g, '')
+    }
+  }).filter(x=>x.length).join('/')
+}
