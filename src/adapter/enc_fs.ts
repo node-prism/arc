d6e0e5aab8a592fc62af39d6fa8b2e33c6854985
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-26 20:47:32 -0600

    Refactor key assignment for enc_fs.

--- src/adapter/enc_fs.ts
+++ src/adapter/enc_fs.ts
@@ -9,8 +9,9 @@ export default class EncryptedFSAdapter<T> implements StorageAdapter<T> {
   name: string;
   filePath: string;
   queue: SimpleFIFO;
+  key: string;
 
-  constructor(storagePath: string, name: string) {
+  constructor(storagePath: string, name: string, key: string = "Mahpsee2X7TKLe1xwJYmar91pCSaZIY7") {
     if (!name.endsWith(".json")) {
       name += ".json";
     }
@@ -19,6 +20,7 @@ export default class EncryptedFSAdapter<T> implements StorageAdapter<T> {
     this.name = name;
     this.queue = new SimpleFIFO();
     this.filePath = path.join(this.storagePath, this.name);
+    this.key = key;
     this.prepareStorage();
   }
 
@@ -35,7 +37,7 @@ export default class EncryptedFSAdapter<T> implements StorageAdapter<T> {
   read(): { [key: string]: T } {
     try {
       const data = fs.readFileSync(this.filePath, "utf8");
-      const decrypted = decrypt(data);
+      const decrypted = decrypt(data, this.key);
 
       return Object.assign(
         {},
@@ -49,7 +51,7 @@ export default class EncryptedFSAdapter<T> implements StorageAdapter<T> {
   write(data: { [key: string]: T }) {
     this.queue.push([
       (d: { [key: string]: T }) => {
-        encryptAndWrite(d, this.filePath);
+        encryptAndWrite(d, this.key, this.filePath);
       },
       data,
     ]);
@@ -61,11 +63,9 @@ export default class EncryptedFSAdapter<T> implements StorageAdapter<T> {
   }
 }
 
-const ENCRYPTION_KEY = String(process.env.ARC_ENCFS_KEY || "Mahpsee2X7TKLe1xwJYmar91pCSaZIY7")
-
-const encryptAndWrite = (data: any, ...args: any[]) => {
+const encryptAndWrite = (data: any, key: string, ...args: any[]) => {
   const json = JSON.stringify(data, null, 0);
-  return write(encrypt(json), ...args);
+  return write(encrypt(json, key), ...args);
 };
 
 const write = (data: string, ...args: any) => {
@@ -78,9 +78,9 @@ const write = (data: string, ...args: any) => {
   * containing the encrypted data, the initialization vector
   * and the authentication tag.
   */
-const encrypt = (text: string) => {
+const encrypt = (text: string, key: string) => {
   const iv = Buffer.from(crypto.randomBytes(16)).toString("hex").slice(0, 16);
-  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(key), iv);
   const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
   return `${iv}:${encrypted.toString("hex")}`;
 };
@@ -91,10 +91,10 @@ const encrypt = (text: string) => {
  * vector and the authentication tag and decrypts it    
  * using the aes-256-cbc algorithm. 
  */
-const decrypt = (text: string) => {
+const decrypt = (text: string, key: string) => {
   const textParts = text.includes(":") ? text.split(":") : [];
   const iv = Buffer.from(textParts.shift() || "", "binary");
   const encryptedtext = Buffer.from(textParts.join(":"), "hex");
-  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(key), iv);
   return Buffer.concat([decipher.update(encryptedtext), decipher.final()]).toString();
 };
