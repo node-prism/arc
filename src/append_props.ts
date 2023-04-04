10ff3a32dfdc16d656e410f9f1faf3443129fe5d
Jonathan Pyers <pyersjonathan@gmail.com>
2023-04-03 20:09:31 -0600

    Refactor and add comments.

--- src/append_props.ts
+++ src/append_props.ts
@@ -2,13 +2,31 @@ import _ from "lodash";
 import { checkAgainstQuery } from "./return_found";
 import { isEmptyObject, isObject, Ok } from "./utils";
 
+/**
+ * Appends newProps to objects and arrays within source if they match the query.
+ * @param source - object or array of objects to append properties to
+ * @param query - object with properties to match against
+ * @param newProps - properties to append to matching objects
+ * @param merge - whether to merge matching objects with newProps instead of replacing
+ * @returns source with newProps appended to matching objects
+ */
 export function appendProps(source: any, query: object, newProps: any, merge = false) {
+  // If source is undefined, return undefined
   if (source === undefined) return undefined;
 
+  /**
+   * Recursively processes objects to append newProps to matching objects
+   * @param item - object or array to process
+   * @returns object or array with newProps appended to matching objects
+   */
   const processObject = (item: any) => {
+    // If item is not an object or array, return it as is
     if (!isObject(item)) return item;
 
+    // Clone the item to avoid modifying the original
     const clone = _.cloneDeep(item);
+    
+    // If the clone matches the query, append or merge newProps
     if (checkAgainstQuery(clone, query)) {
       if (!merge) {
         Object.assign(clone, newProps);
@@ -17,18 +35,21 @@ export function appendProps(source: any, query: object, newProps: any, merge = f
       }
     }
 
+    // Recursively process child objects and arrays
     for (const key of Object.keys(clone)) {
       if (isObject(clone[key]) || Array.isArray(clone[key])) {
-        clone[key] = appendProps(clone[key], query, newProps);
+        clone[key] = processObject(clone[key]);
       }
     }
 
     return clone;
   };
 
+  // If source is an array or object and query and newProps are not empty, process source
   if ((Array.isArray(source) || isObject(source)) && !isEmptyObject(query) && !isEmptyObject(newProps)) {
     return Array.isArray(source) ? source.map(processObject) : processObject(source);
   }
 
+  // Otherwise, return source as is
   return source;
 }
--- src/change_props.ts
+++ src/change_props.ts
@@ -1,27 +1,45 @@
+import _ from "lodash";
 import { checkAgainstQuery } from "./return_found";
 import { isEmptyObject, isObject, Ok, safeHasOwnProperty } from "./utils";
 
-export const changeProps = (
-  source: any,
-  query: object,
-  replaceProps: { [x: string]: any },
+/**
+ * Recursively replaces properties of a source object or array based on a query object.
+ *
+ * @param source - The object or array to process.
+ * @param query - The properties to match.
+ * @param replaceProps - The replacement properties.
+ * @param createNewProperties - Whether to create new properties if they don't exist.
+ * @returns The processed object or array.
+ */
+export const changeProps = <T>(
+  source: T,
+  query: Partial<T>,
+  replaceProps: Partial<T>,
   createNewProperties = false
-) => {
-  if (source === undefined) return undefined;
+): T | undefined => {
+  if (!source) return undefined;
 
+  // helper function to process objects and arrays recursively
   const processObject = (item: any) => {
+    // if item is not an object, return item
     if (!isObject(item)) return item;
 
-    const itemClone = { ...item };
+    // create a clone of the item
+    const itemClone = _.cloneDeep(item);
 
+    // loop through replaceProps object keys
     for (const key of Object.keys(replaceProps)) {
+      // if itemClone matches query and createNewProperties is true or the key already exists in itemClone
       if (checkAgainstQuery(itemClone, query) &&
           (createNewProperties || safeHasOwnProperty(itemClone, key))) {
+        // update the itemClone key with the new value from replaceProps
         itemClone[key] = replaceProps[key];
       }
     }
 
+    // loop through itemClone keys
     for (const key of Object.keys(itemClone)) {
+      // if the value of the key is an object or an array, call processObject recursively
       if (isObject(itemClone[key]) || Array.isArray(itemClone[key])) {
         itemClone[key] = changeProps(
           itemClone[key],
@@ -32,18 +50,18 @@ export const changeProps = (
       }
     }
 
+    // return the updated itemClone
     return itemClone;
   };
 
-  if (
-    (Array.isArray(source) || isObject(source)) &&
-    !isEmptyObject(query) &&
-    !isEmptyObject(replaceProps)
-  ) {
-    return Array.isArray(source)
-      ? source.map(processObject)
-      : processObject(source);
+  // if source is an object and both query and replaceProps are not empty objects, call processObject
+  if (isObject(source) && !isEmptyObject(query) && !isEmptyObject(replaceProps)) {
+    return processObject(source);
+  // if source is an array and both query and replaceProps are not empty objects, map through the array and call processObject on each item
+  } else if (Array.isArray(source) && !isEmptyObject(query) && !isEmptyObject(replaceProps)) {
+    return source.map(processObject) as unknown as T;
+  // otherwise, return the original source
+  } else {
+    return source;
   }
-
-  return source;
 };
